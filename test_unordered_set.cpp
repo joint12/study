ec78366a7a30501673990b1ae2ed1e0e49090cd1
joint12 <96482640+joint12@users.noreply.github.com>
2021-12-22 00:18:55 +0900

    unordered_set 추가

--- test_unordered_set.cpp
+++ test_unordered_set.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
 
 int main()
 {
-	std::set<int> myset;
+	std::unordered_set<int> myset;
 
-	myset.insert(1);
-	myset.insert(5);
-	myset.insert(10);
 	myset.insert(20);
+	myset.insert(10);
+	myset.insert(1000);
+	myset.insert(100);
+	myset.insert(5);
+	myset.insert(1);
 
 	for (auto it = myset.begin(); it != myset.end(); ++it)
 		std::cout << *it << ' ';
@@ -24,7 +26,9 @@ int main()
 }
 
 /*
-1 5 10 20
-1 5 20
-size: 3
+100 20 10 1 1000 5
+100 20 1 1000 5
+size: 5
 */
+
+// unordered_set은 set과 달리 정렬이 안됨
