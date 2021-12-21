1744dc10d92786ff5ceb637801740cb4f14c58bf
joint12 <96482640+joint12@users.noreply.github.com>
2021-12-22 00:45:42 +0900

    test_sort_vector_array 수정

--- test_sort_vector_array.cpp
+++ test_sort_vector_array.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <array>
 
 int main()
 {
@@ -15,9 +16,25 @@ int main()
 	for (int i = 0; i < 10; ++i)
 		std::cout << myvec[i] << ' ';
 	std::cout << std::endl;
+
+	/////////////////////////////////////////////////
+
+	std::array<int, 10> myarr{ 2,4,6,8,10,1,3,5,7,9 };
+
+	for (int i = 0; i < 10; ++i)
+		std::cout << myarr[i] << ' ';
+	std::cout << std::endl;
+
+	std::sort(myarr.begin(), myarr.end());
+
+	for (int i = 0; i < 10; ++i)
+		std::cout << myarr[i] << ' ';
+	std::cout << std::endl;
 }
 
 /*
+2 4 6 8 10 1 3 5 7 9
+1 2 3 4 5 6 7 8 9 10
 2 4 6 8 10 1 3 5 7 9
 1 2 3 4 5 6 7 8 9 10
 */
