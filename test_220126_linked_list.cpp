af64df062b7a69e9484a47c9c5967475c36b708f
joint12 <96482640+joint12@users.noreply.github.com>
2022-01-26 13:44:30 +0900

    linked list 추가

---
+++ test_220126_linked_list.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+
+struct Node
+{
+	int data;
+	Node* nextNode;
+};
+
+class LinkedList
+{
+private:
+	Node* head;
+	Node* tail;
+
+public:
+	LinkedList()
+	{
+		// head와 tail의 포인터를 초기화
+		head = NULL;
+		tail = NULL;
+	}
+
+	void AddFrontNode(int n);
+	void AddNode(int n);
+	void InsertNode(Node* prevNode, int n);
+	void DeleteNode(Node* prevNode);
+
+	Node* GetHead()
+	{
+		return head;
+	}
+
+	void Display(Node* head);
+};
+
+void LinkedList::AddFrontNode(int n)
+{
+	Node* temp = new Node;
+	temp->data = n;
+
+	if (head == NULL)
+	{
+		head = temp;
+		tail = temp;
+	}
+	else
+	{
+		temp->nextNode = head;
+		head = temp;
+	}
+}
+
+void LinkedList::AddNode(int n)
+{
+	Node* temp = new Node;
+
+	temp->data = n;
+	temp->nextNode = NULL;
+
+	if (head == NULL)
+	{
+		head = temp;
+		tail = temp;
+	}
+	else
+	{
+		tail->nextNode = temp;
+		tail = temp;
+	}
+}
+
+void LinkedList::InsertNode(Node* prevNode, int n)
+{
+	Node* temp = new Node;
+
+	temp->data = n;
+
+	temp->nextNode = prevNode->nextNode;
+
+	prevNode->nextNode = temp;
+}
+
+void LinkedList::DeleteNode(Node* prevNode)
+{
+	Node* temp = prevNode->nextNode;
+
+	prevNode->nextNode = temp->nextNode;
+
+	delete temp;
+}
+
+void LinkedList::Display(Node* head)
+{
+	if (head == NULL)
+	{
+		std::cout << "\n";
+	}
+	else
+	{
+		std::cout << head->data << " ";
+		Display(head->nextNode);
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	LinkedList a;
+
+	a.AddNode(1);
+	a.AddNode(2);
+	a.AddNode(3);
+	a.Display(a.GetHead());
+
+	a.AddFrontNode(0);
+	a.InsertNode(a.GetHead()->nextNode->nextNode, 9);
+	a.Display(a.GetHead());
+
+	a.DeleteNode(a.GetHead()->nextNode);
+	a.Display(a.GetHead());
+}
