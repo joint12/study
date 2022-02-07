499f3f1cf3199e9253e85c52966c61ba5822f5db
joint12 <96482640+joint12@users.noreply.github.com>
2022-02-07 19:42:00 +0900

    priority_queue 2

--- test_220207_2_priority_queue.cpp
+++ test_220207_2_priority_queue.cpp
@@ -4,17 +4,44 @@
 #include <iostream>       // std::cout
 #include <queue>          // std::priority_queue
 
-int main ()
+/*
+template<typename T>
+void print_queue(T q) { // NB: pass by value so the print uses a copy
+	while (!q.empty()) {
+		std::cout << q.top() << ' ';
+		q.pop();
+	}
+	std::cout << '\n';
+}
+*/
+
+void print_queue(std::priority_queue<int> q) { // NB: pass by value so the print uses a copy
+	while (!q.empty()) {
+		std::cout << q.top() << ' ';
+		q.pop();
+	}
+	std::cout << '\n';
+}
+
+int main()
 {
-  std::priority_queue<int> mypq;
+	std::priority_queue<int> mypq;
 
-  mypq.push(10);
-  mypq.push(20);
-  mypq.push(15);
+	mypq.push(10);
+	mypq.push(20);
+	mypq.push(15);
+	mypq.push(1);
+	mypq.push(4);
+	mypq.push(9);
 
-  std::cout << "mypq.top() is now " << mypq.top() << '\n';
+	std::cout << "mypq.top() is now " << mypq.top() << '\n';
 
-  return 0;
+
+	std::cout << "--------------" << '\n';
+
+	print_queue(mypq);
+
+	return 0;
 }
 
 ///////////////////////////////////
