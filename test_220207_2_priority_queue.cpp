d8717c958258025cc9bd86931e5ae9ea278a2721
joint12 <96482640+joint12@users.noreply.github.com>
2022-02-07 19:37:44 +0900

    priority_queue

---
+++ test_220207_2_priority_queue.cpp
@@ -0,0 +1,22 @@
+// https://www.cplusplus.com/reference/queue/priority_queue/top/
+
+// priority_queue::top
+#include <iostream>       // std::cout
+#include <queue>          // std::priority_queue
+
+int main ()
+{
+  std::priority_queue<int> mypq;
+
+  mypq.push(10);
+  mypq.push(20);
+  mypq.push(15);
+
+  std::cout << "mypq.top() is now " << mypq.top() << '\n';
+
+  return 0;
+}
+
+///////////////////////////////////
+// Output:
+// mypq.top() is now 20
