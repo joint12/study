9790f696908d685195dc2ade5cb34e65c67ba3f0
joint12 <96482640+joint12@users.noreply.github.com>
2022-02-09 23:22:13 +0900

    strxxx 추가

---
+++ test_220209_strxxx_series.cpp
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	char ch1[20] = "abc";
+	char ch2[20] = "hello";
+
+	int length1 = strlen(ch1);
+	int length2 = strlen(ch2);
+
+	std::cout << "length1: " << length1 << std::endl;
+	std::cout << "length2: " << length2 << std::endl;
+
+	std::cout << "-----------------------------" << std::endl;
+	///////////////////////////////////////////////////////
+
+	char ch3[20] = "abc";
+	char ch4[20];
+
+	std::cout << "ch3: " << ch3 << std::endl;
+	std::cout << "ch4: " << ch4 << std::endl;
+
+	strcpy(ch4, ch3);
+
+	std::cout << "ch3: " << ch3 << std::endl;
+	std::cout << "ch4: " << ch4 << std::endl;
+
+	std::cout << "-----------------------------" << std::endl;
+	///////////////////////////////////////////////////////
+
+	char ch5[20] = "ab";
+	char ch6[20] = "cd";
+	char ch7[20] = "ef";
+
+	std::cout << "ch5 : " << ch5 << std::endl;
+	strcat(ch5, ch6);
+	std::cout << "ch5 : " << ch5 << std::endl;
+	strcat(ch5, ch7);
+	std::cout << "ch5 : " << ch5 << std::endl;
+
+	std::cout << "-----------------------------" << std::endl;
+	///////////////////////////////////////////////////////
+
+	char ch8[20] = "ab";
+	char ch9[20] = "ab";
+	char ch10[20] = "cd";
+
+	// strcmp는 같으면 false
+	if (!strcmp(ch8, ch9))
+		std::cout << "111" << std::endl;
+	else
+		std::cout << "222" << std::endl;
+
+	if (!strcmp(ch8, ch10))
+		std::cout << "333" << std::endl;
+	else
+		std::cout << "444" << std::endl;
+
+	std::cout << "-----------------------------" << std::endl;
+	///////////////////////////////////////////////////////
+
+	return 0;
+}
+
+// https://m.blog.naver.com/zlatmgpdjtiq/221661570066
+
+// output : 
+// 
+// length1: 3
+// length2 : 5
+// ---------------------------- -
+// ch3 : abc
+// ch4 : 儆儆儆儆儆儆儆儆儆儆儆儆儆儆儆儆儆儆儆儆
+// ch3 : abc
+// ch4 : abc
+// ---------------------------- -
+// ch5 : ab
+// ch5 : abcd
+// ch5 : abcdef
+// ---------------------------- -
+// 111
+// 444
+// ---------------------------- -
