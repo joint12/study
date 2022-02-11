74eff6e312b127973186fef572211fbe20eac6c1
joint12 <96482640+joint12@users.noreply.github.com>
2022-02-12 00:28:30 +0900

    test_220212_set_string

---
+++ test_220212_set_string.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+int main()
+{
+	std::set<std::string> s;
+
+	char ch1[10] = "hello";
+	char ch2[10] = "world";
+
+	s.insert(ch1);
+	s.insert(ch2);
+
+	std::cout << "s.size(): " << s.size() << std::endl;
+
+	for (const auto& ele : s)
+	{
+		std::cout << ele << std::endl;
+	}
+
+	return 0;
+}
+
+// s.size() : 2
+// hello
+// world
