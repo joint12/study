fc84e8779cd1a36c91be275f5c5b74b280685e9e
joint12 <96482640+joint12@users.noreply.github.com>
2022-02-12 00:33:31 +0900

    test_220212_set_string 2

--- test_220212_set_string.cpp
+++ test_220212_set_string.cpp
@@ -19,9 +19,25 @@ int main()
 		std::cout << ele << std::endl;
 	}
 
+	/////////////////////////////////////////////////////////
+
+	std::set<std::string>::iterator it1 = s.find(ch1);
+	if (it1 != s.end())
+		std::cout << "it1: " << *it1 << std::endl;
+
+	std::set<std::string>::iterator it2 = s.find("hel");
+	if (it2 != s.end())
+		std::cout << "it2: " << *it2 << std::endl;
+
+	std::set<std::string>::iterator it3 = s.find("hello");
+	if (it3 != s.end())
+		std::cout << "it3: " << *it3 << std::endl;
+
 	return 0;
 }
 
 // s.size() : 2
 // hello
 // world
+// it1 : hello
+// it3 : hello
